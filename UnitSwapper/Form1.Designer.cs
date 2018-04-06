fe3043e397d0f7b55f8a6038d78143c5cb63c601
kelvinwop <30770221+kelvinwop@users.noreply.github.com>
2018-04-05 23:10:21 -0500

    Initial commit

---
+++ UnitSwapper/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace UnitSwapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_InputValue = new System.Windows.Forms.TextBox();
+            this.txt_InputUnits = new System.Windows.Forms.TextBox();
+            this.txt_OutputValue = new System.Windows.Forms.TextBox();
+            this.txt_OutputUnits = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_Calculate = new System.Windows.Forms.Button();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_InputValue
+            // 
+            this.txt_InputValue.Location = new System.Drawing.Point(60, 121);
+            this.txt_InputValue.Name = "txt_InputValue";
+            this.txt_InputValue.Size = new System.Drawing.Size(121, 20);
+            this.txt_InputValue.TabIndex = 0;
+            // 
+            // txt_InputUnits
+            // 
+            this.txt_InputUnits.Location = new System.Drawing.Point(230, 121);
+            this.txt_InputUnits.Name = "txt_InputUnits";
+            this.txt_InputUnits.Size = new System.Drawing.Size(121, 20);
+            this.txt_InputUnits.TabIndex = 1;
+            // 
+            // txt_OutputValue
+            // 
+            this.txt_OutputValue.Location = new System.Drawing.Point(505, 121);
+            this.txt_OutputValue.Name = "txt_OutputValue";
+            this.txt_OutputValue.Size = new System.Drawing.Size(210, 20);
+            this.txt_OutputValue.TabIndex = 2;
+            // 
+            // txt_OutputUnits
+            // 
+            this.txt_OutputUnits.Location = new System.Drawing.Point(755, 121);
+            this.txt_OutputUnits.Name = "txt_OutputUnits";
+            this.txt_OutputUnits.Size = new System.Drawing.Size(121, 20);
+            this.txt_OutputUnits.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Input Value*";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(230, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Input Units*";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(505, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Output Value";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(755, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Output Units*";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(60, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(176, 52);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Make sure units are space delimited\r\n\r\nEg:\r\nkg m s^-3";
+            // 
+            // btn_Calculate
+            // 
+            this.btn_Calculate.Location = new System.Drawing.Point(367, 186);
+            this.btn_Calculate.Name = "btn_Calculate";
+            this.btn_Calculate.Size = new System.Drawing.Size(75, 23);
+            this.btn_Calculate.TabIndex = 9;
+            this.btn_Calculate.Text = "Calculate";
+            this.btn_Calculate.UseVisualStyleBackColor = true;
+            this.btn_Calculate.Click += new System.EventHandler(this.btn_Calculate_Click);
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Location = new System.Drawing.Point(471, 186);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(75, 23);
+            this.btn_Clear.TabIndex = 10;
+            this.btn_Clear.Text = "Clear";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 270);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.btn_Calculate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_OutputUnits);
+            this.Controls.Add(this.txt_OutputValue);
+            this.Controls.Add(this.txt_InputUnits);
+            this.Controls.Add(this.txt_InputValue);
+            this.Name = "Form1";
+            this.Text = "Kelvin\'s Awesome Unit Changer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_InputValue;
+        private System.Windows.Forms.TextBox txt_InputUnits;
+        private System.Windows.Forms.TextBox txt_OutputValue;
+        private System.Windows.Forms.TextBox txt_OutputUnits;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_Calculate;
+        private System.Windows.Forms.Button btn_Clear;
+    }
+}
+
---
+++ UnitSwapper/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnitSwapper
+{
+    public enum BaseType { ElectricalCurrent, Luminosity, Temperature, Mass, Length, Amount, Time };
+
+    public partial class Form1 : Form
+    {
+        public Dictionary<string, BaseType> UnitType = new Dictionary<string, BaseType>()
+        {
+            {"kg", BaseType.Mass},
+            {"slug", BaseType.Mass},
+            {"g", BaseType.Mass},
+            {"mg", BaseType.Mass},
+            {"lbm", BaseType.Mass}, //pound_mass
+            {"m", BaseType.Length},
+            {"cm", BaseType.Length},
+            {"mm", BaseType.Length},
+            {"km", BaseType.Length},
+            {"in", BaseType.Length},
+            {"mi", BaseType.Length},
+            {"yd", BaseType.Length},
+            {"ft", BaseType.Length},
+            {"a", BaseType.ElectricalCurrent}, //ampere
+            {"day", BaseType.Time},
+            {"h", BaseType.Time},
+            {"min", BaseType.Time},
+            {"s", BaseType.Time},
+            {"ms", BaseType.Time},
+            {"week", BaseType.Time},
+            {"month", BaseType.Time},
+            {"k", BaseType.Temperature},
+            {"c", BaseType.Temperature},
+            {"f", BaseType.Temperature},
+            {"r", BaseType.Temperature},
+            {"cd", BaseType.Luminosity}, //candela
+            {"mol", BaseType.Amount} //mole
+        };
+
+        public Dictionary<string, double> ConversionRatios = new Dictionary<string, double>()
+        {
+            {"kg", 1000},
+            {"slug", 14593.9},
+            {"g", 1},
+            {"mg", 0.001},
+            {"lbm", 453.592}, //pound_mass
+            {"m", 1},
+            {"cm", 0.01},
+            {"mm", 0.001},
+            {"km", 1000},
+            {"in", 0.0254},
+            {"mi", 1609.34},
+            {"yd", 0.9144},
+            {"ft", 0.3048},
+            {"a", 1}, //ampere
+            {"day", 86400},
+            {"h", 3600},
+            {"min", 60},
+            {"s", 1},
+            {"ms", 0.001},
+            {"week", 604800},
+            {"month", 2.628e6},
+            {"k", 1},
+            {"c", 1},
+            {"f", 1.8},
+            {"r", 1.8},
+            {"cd", 1}, //candela
+            {"mol", 1}, //mole
+        };
+
+        float inputAmount;
+        string inputUnits;
+        string outputUnits;
+        List<Unit> inputUnitList;
+        List<Unit> outputUnitList;
+        int[] inputProfile;
+        int[] outputProfile;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Calculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                inputAmount = Int32.Parse(txt_InputValue.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Amount box value invalid (Could not parse to floating point)", "Error!");
+                return;
+            }
+            inputUnits = txt_InputUnits.Text;
+            outputUnits = txt_OutputUnits.Text;
+
+            //read text into unit lists
+            inputUnitList = readUnits(inputUnits);
+            outputUnitList = readUnits(outputUnits);
+
+            //make sure same number of each units in input and output
+            inputProfile = profileUnits(inputUnitList);
+            outputProfile = profileUnits(outputUnitList);
+
+            if(!Enumerable.SequenceEqual(inputProfile,outputProfile))
+            {
+                MessageBox.Show("Quantity of each unit type does not match. Please check input and output.", "Error");
+                return;
+            }
+
+            //convert and evangelize the metric
+            double scalarIN = 1;
+            string log = "";
+            foreach(Unit u in inputUnitList)
+            {
+                double value;
+                ConversionRatios.TryGetValue(u.name, out value);
+                scalarIN *= Math.Pow(value,u.number);
+                log += u.name + "||";
+            }
+            double scalarOUT = 1;
+            foreach (Unit u in outputUnitList)
+            {
+                double value;
+                ConversionRatios.TryGetValue(u.name, out value);
+                scalarOUT *= Math.Pow(value, u.number);
+            }
+
+            txt_OutputValue.Text = "" + scalarIN / scalarOUT * inputAmount;
+        }
+
+        int[] profileUnits(List<Unit> list)
+        {
+            int[] output = new int[7];
+            foreach(Unit u in list)
+            {
+                if (u.number > 0)
+                    output[(int)u.type]++;
+                else
+                    output[(int)u.type]--;
+            }
+            return output;
+        }
+
+        List<Unit> readUnits(string list)
+        {
+            List<Unit> output = new List<Unit>();
+            foreach(string s in list.Split(' '))
+            {
+                BaseType value;
+                if(UnitType.TryGetValue(s.Split('^')[0], out value))
+                {
+                    if (s.Contains("^"))
+                    {
+                        output.Add(new Unit(s.Split('^')[0], value, Int32.Parse(s.Split('^')[1])));
+                    }
+                    else
+                    {
+                        output.Add(new Unit(s, value));
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Could not identify " + s, "Error");
+                    return new List<Unit>();
+                }
+            }
+
+            //remove duplicates
+            var duplicateUnits = output.GroupBy(x => x).Where(group => group.Count() > 1).Select(group => group.Key);
+
+            foreach(var d in duplicateUnits)
+            {
+                int count = 0;
+                foreach (Unit u in output)
+                {
+                    if(u.Equals(d))
+                    {
+                        count += u.number;
+                    }
+                }
+                output.RemoveAll(x => x.Equals(d));
+                if(count != 0)
+                {
+                    output.Add(new Unit(d.name, d.type, count));
+                }
+            }
+            return output;
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            txt_OutputValue.Text = "";
+            txt_OutputUnits.Text = "";
+            txt_InputValue.Text = "";
+            txt_InputUnits.Text = "";
+        }
+    }
+
+    public class Unit
+    {
+        public string name; //eg. km, s, mi, etc...
+        public BaseType type;
+        public int number;
+        public Unit(string name, BaseType type)
+        {
+            this.name = name;
+            this.type = type;
+            this.number = 1;
+        }
+        public Unit(string name, BaseType type, int number)
+        {
+            this.name = name;
+            this.type = type;
+            this.number = number;
+        }
+        public override bool Equals(object obj)
+        {
+            Unit obj2 = (Unit)obj;
+            return (obj2.name == this.name && obj2.type == this.type);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
